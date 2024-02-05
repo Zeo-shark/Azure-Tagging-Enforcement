36a8e89638650f16dd123ca39b7441ef49560dd9
Zeo-shark <sauravberaiit@gmail.com>
2024-02-05 07:26:50 +0530

    Added template index.html file for portfolio

--- Import Export Tags/import_Azure_Resource_tags.ps1
+++ Import Export Tags/import_Azure_Resource_tags.ps1
@@ -1,6 +1,17 @@
 Import-Csv -Path azureresources.csv | ForEach-Object {
+    # Convert the tags value from Json to a custom object
     $tags = $_.Tags | ConvertFrom-Json
+    # Check if the tags value is not null
     if ($tags -ne $null) {
-        Set-AzResourceGroup -Name $_.ResourceGroupName -Tag $tags
+        # Create an empty hashtable
+        $hashtable = @{}
+        # Loop through the properties of the custom object and add them to the hashtable
+        $tags.psobject.properties | ForEach-Object {
+            $hashtable[$_.Name] = $_.Value
+        }
+        "Setting tags values for the $_ and $hashtable" 
+        # Set the resource group with the hashtable as the tag parameter
+        Set-AzContext -Subscription $_.SubscriptionName
+        Set-AzResourceGroup -Id $_.ResourceId -Tag $hashtable 
     }
 }
